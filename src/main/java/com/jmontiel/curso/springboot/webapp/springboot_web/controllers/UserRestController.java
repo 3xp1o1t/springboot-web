b176c90efff86151bd5c796b9a7ff8767d0b8160
JESUS SALVADOR MONTIEL SANCHEZ <jesus.montiel@elektra.com.mx>
2025-07-01 13:02:00 -0600

    add: working with DTOs

--- src/main/java/com/jmontiel/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/jmontiel/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -1,25 +1,33 @@
 package com.jmontiel.curso.springboot.webapp.springboot_web.controllers;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jmontiel.curso.springboot.webapp.springboot_web.models.User;
-
+import com.jmontiel.curso.springboot.webapp.springboot_web.models.dto.UserDto;
 
 @RestController
 @RequestMapping("/api")
 public class UserRestController {
 
+  // Using DTOs
   @GetMapping("/details")
-  public Map<String, Object> details() {
+  public UserDto details() {
+    UserDto userDto = new UserDto();
     User user = new User("Jesus", "Montiel");
-    Map<String, Object> model = new HashMap<>();
-    model.put("title", "Parametros desde controlador");
-    model.put("user", user);
-    return model;
+    userDto.setTitle("Hola mundo usando DTOs");
+    userDto.setUser(user);
+    return  userDto;
   }
+
+  // USING MAP
+  // @GetMapping("/details")
+  // public Map<String, Object> details() {
+  //   User user = new User("Jesus", "Montiel");
+  //   Map<String, Object> model = new HashMap<>();
+  //   model.put("title", "Parametros desde controlador");
+  //   model.put("user", user);
+  //   return model;
+  // }
 }
