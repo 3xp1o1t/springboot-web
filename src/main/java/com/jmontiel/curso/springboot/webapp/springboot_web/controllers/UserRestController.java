9b4288ee022d8b4d4d3975dd2d80e433193af32d
JESUS SALVADOR MONTIEL SANCHEZ <jesus.montiel@elektra.com.mx>
2025-07-01 11:59:35 -0600

    add: request mapping default endpoints

--- src/main/java/com/jmontiel/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/jmontiel/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -4,13 +4,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequestMapping("/api")
 public class UserRestController {
 
-  @GetMapping("/details2")
+  @GetMapping("/details")
   public Map<String, Object> details() {
     Map<String, Object> model = new HashMap<>();
     model.put("title", "Parametros desde controlador");
