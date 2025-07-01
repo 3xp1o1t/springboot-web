5f79ccccc5e911bad52ba034cebfb98a9d8fcaeb
JESUS SALVADOR MONTIEL SANCHEZ <jesus.montiel@elektra.com.mx>
2025-07-01 12:17:39 -0600

    add: working with models

--- src/main/java/com/jmontiel/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/jmontiel/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -4,14 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.jmontiel.curso.springboot.webapp.springboot_web.models.User;
+
 @Controller
 public class UserController {
 
   @GetMapping("/details")
   public String details(Model model) {
+    User user = new User("Jesus", "Montiel");
     model.addAttribute("title", "Parametros desde controlador");
-    model.addAttribute("name", "Jesus");
-    model.addAttribute("lastname", "Montiel");
+    model.addAttribute("user", user);
     return "details";
   }
 }
--- src/main/java/com/jmontiel/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/jmontiel/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jmontiel.curso.springboot.webapp.springboot_web.models.User;
+
 
 @RestController
 @RequestMapping("/api")
@@ -14,10 +16,10 @@ public class UserRestController {
 
   @GetMapping("/details")
   public Map<String, Object> details() {
+    User user = new User("Jesus", "Montiel");
     Map<String, Object> model = new HashMap<>();
     model.put("title", "Parametros desde controlador");
-    model.put("name", "Jesus");
-    model.put("lastname", "Montiel");
+    model.put("user", user);
     return model;
   }
 }
