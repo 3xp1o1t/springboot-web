fd5203030f7245e94ae6509ece72f46176c9dbce
JESUS SALVADOR MONTIEL SANCHEZ <jesus.montiel@elektra.com.mx>
2025-07-03 17:03:18 -0600

    add: RequestParam example with default props

---
+++ src/main/java/com/jmontiel/curso/springboot/webapp/springboot_web/controllers/RequestParamController.java
@@ -0,0 +1,22 @@
+package com.jmontiel.curso.springboot.webapp.springboot_web.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jmontiel.curso.springboot.webapp.springboot_web.models.dto.ParamDto;
+
+
+@RestController
+@RequestMapping("/api/params")
+public class RequestParamController {
+
+  @GetMapping("/foo")
+  public ParamDto foo(@RequestParam(required = false, defaultValue = "I think u miss something", name="mensaje") String message) {
+    ParamDto param = new ParamDto();
+    param.setMessage(message);
+    return param;
+  }
+
+}
